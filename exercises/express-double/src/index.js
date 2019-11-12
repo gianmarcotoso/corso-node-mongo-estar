6465e80012aeb00c8913c99ee96caf6d9d913cfe
Gian Marco Toso <info@gianmarcotoso.com>
2019-11-12 09:45:16 +0100

    update exercises

--- exercises/express-double/src/index.js
+++ exercises/express-double/src/index.js
@@ -1,29 +1,17 @@
-/* Esempio: Upload di un file */
-
-// ATTENZIONE! Questo codice è esclusivamente
-// a scopo esemplificativo e non è sicuro.
-// --> NON USARE IN PRODUZIONE! <--
-
 const express = require('express')
-const multer = require('multer')
-const { rename } = require('fs')
-const { promisify } = require('util')
-
-const upload = multer({ dest: 'uploads/' })
 const app = express()
+const bodyParser = require('body-parser')
+
 const port = 3000
 
-const renameAsync = promisify(rename)
+app.use(bodyParser.json())
 
-app.post('/', upload.single('image'), async (req, res, next) => {
-	await renameAsync(
-		`uploads/${req.file.filename}`,
-		`uploads/${req.file.originalname}`,
-	)
+app.post('/double', function(req, res, next) {
+	const { number } = req.body
 
-	return res.send(`Ok!`)
+	res.send(`Your number is: ${number * 2}`)
 })
 
-app.listen(port, () => {
-	console.log(`Upload app listening on port ${port}!`)
+app.listen(port, function() {
+	console.log(`Application listening on port ${port}`)
 })
--- exercises/express-multer/src/index.js
+++ exercises/express-multer/src/index.js
@@ -1,41 +1,20 @@
-const users = require('./users.json')
 const express = require('express')
-const loggingMiddleware = require('./middleware/logging.middleware')
-const fileLoggingMiddleware = require('./middleware/file-logging.middleware')
-const randomAccessMiddleware = require('./middleware/random-access.middleware')
+const multer = require('multer')
+const { rename } = require('fs')
+const { promisify } = require('util')
 
-const errorHandler = require('./middleware/error-handler.middleware')
-const errorLogger = require('./middleware/error-logger.middleware')
-
+const upload = multer({ dest: 'uploads/' })
 const app = express()
 const port = 3000
 
-app.use(loggingMiddleware, fileLoggingMiddleware)
+const renameAsync = promisify(rename)
 
-app.get('/', [
-	randomAccessMiddleware,
-	(req, res) => {
-		res.send('Hello')
-	},
-])
+app.post('/', upload.single('image'), async (req, res, next) => {
+	await renameAsync(`uploads/${req.file.filename}`, `uploads/${req.file.originalname}`)
 
-app.get('/users/count', (req, res, next) => {
-	const count = users.length
-
-	return res.send(`Total users: ${count}`)
+	return res.send(`Ok!`)
 })
-
-app.get('/users/:id', (req, res, next) => {
-	const user = users.find(u => u.id === +req.params.id)
-	if (!user) {
-		return next(new Error('User not found'))
-	}
-
-	return res.send(user.name)
-})
-
-app.use([errorLogger, errorHandler])
 
 app.listen(port, () => {
-	console.log(`Example app listening on port ${port}!`)
+	console.log(`Upload app listening on port ${port}!`)
 })
